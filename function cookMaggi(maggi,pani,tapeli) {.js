8f7c7407ee3cb576870af04649750b8121e053c9
Yuganshi Joshi <yuganshijoshi@gmail.com>
2023-07-21 11:47:06 +0530

    Small task

---
+++ function cookMaggi(maggi,pani,tapeli) {.js
@@ -0,0 +1,9 @@
+function cookMaggi(maggi,pani,tapeli) {
+    console.log("your maggie will be ready in"
+    +maggie*2
+    +"minutes"
+    +"and ingredients used are:-"
+    +pani+"cups of water
+    +"using" +tapeli+ "pan")
+}
+cookMaggi();
